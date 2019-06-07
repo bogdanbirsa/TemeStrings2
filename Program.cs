4b697844ebf2c4a10244b2b67f264de947953293
bogdanbirsa <bogdan.birsa@yahoo.ro>
2019-06-07 22:32:02 +0300

    pct 3 si 4

--- Program.cs
+++ Program.cs
@@ -7,9 +7,12 @@ namespace TemeString2
     {
         static void Main(string[] args)
         {
-            //middleChar();
-            //countChar();
+            middleChar();
+            countChar();
             Like();
+            Console.WriteLine(Ho());
+            Console.WriteLine(Ho(Ho()));
+            Console.WriteLine(Ho(Ho(Ho())));
 
         }
         /*
@@ -83,47 +86,41 @@ namespace TemeString2
 
         static string Like()
         {
+
             string[] name = { " ", "Peter", "Jacob Alex", "Max John Mark", "Alex Jacob Mark Max" };
 
-            Console.WriteLine("Te rog sa sa spui carei persoane dai like? ");
-            Console.Read();
+            if (name.Length == 0)
+                return "No one likes this";
+            if (name.Length == 1)
+                return name[0] + " likes this.";
+            if (name.Length == 2)
+                return name[0] + " and " + name[1] + " like this.";
+            if (name.Length == 3)
+                return name[0] + "," + name[1] + " and " + name[2] + " like this.";
+            return name[0] + ", " + name[1] + " and " + (name.Length - 2).ToString() + " others like this.";
 
-            if (name == null || name.Length == 0)
-            {
-                return "no one likes this";
-            }
-            else if (name.Length == 1)
-            {
-                return $"{name[0]} likes this";
-            }
-            else if (name.Length == 2)
-            {
-                return $"{name[0]} and {name[1]} like this";
-            }
-            else if (name.Length == 3)
-            {
-                return $"{name[0]}, {name[1]} and {name[2]} like this";
-            }
-            return $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
+        }
 
+        /*
+        4. Santa is learning programming. And what could be the first program, he wants to write? Yes, the "Hello world!" of Christmas: 
+        "Ho Ho Ho!".He wants to write a function ho(), which should have the following return values:
+         ho() should return "Ho!" 
+         ho(ho()) should return "Ho Ho!" 
+         ho(ho(ho())) should return "Ho Ho Ho!" 
+         Unfortunately he couldn't find any tutorial, which explains, how he could implement that. Can you help him?
+         Rules: each call of ho() must add a "Ho" to the string the "Ho"'s must be separated by a space at the end of the string, there must be an exclamation mark (!), without a space
+         */
+        static string Ho()
+        {
+            return "Ho!";
+        }
+        static string Ho(string santa)
+        {
+            return "Ho " + santa.ToLower();
         }
     }
 
 }
-
-
-
-
-/*
-4. Santa is learning programming. And what could be the first program, he wants to write? Yes, the "Hello world!" of Christmas: 
-"Ho Ho Ho!".He wants to write a function ho(), which should have the following return values:
- ho(); should return "Ho!" ho(ho());
- should return "Ho Ho!" ho(ho(ho())); 
- should return "Ho Ho Ho!" Unfortunately he couldn't find any tutorial, which explains, how he could implement that. Can you help him?
- Rules:
- each call of ho() must add a "Ho" to the string the "Ho"'s must be separated by a space at the end of the string, there must be an exclamation mark (!), without a space
- */
-
 
 
 
